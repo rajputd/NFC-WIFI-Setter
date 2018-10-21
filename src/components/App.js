6534463df2f29f6ebd755a754fbeac8f1f02a29e
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-20 23:07:00 -0400

    Render proper screen according to App state

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,31 @@ import EnterWifiScreen from '../screens/EnterWifiScreen.js';
 import FailureScreen from '../screens/FailureScreen.js';
 import HomeScreen from '../screens/HomeScreen.js';
 import ProgramTagScreen from '../screens/ProgramTagScreen.js';
+//import { NFC } from 'nfc-pcsc';
 
- class App extends Component{
+//const nfc = new NFC();
+
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       currentScreen: 'Prog',
+       screenParams: {},
+       nfcAvailable: false
+     }
+   }
+
   render() {
+    const currentScreen = this.state.currentScreen;
+    const params = this.state.screenParams;
+
     return (
-      <ProgramTagScreen />
+      <div>
+        {currentScreen === "Home" ? <HomeScreen /> :
+         currentScreen === "Fail" ? <FailureScreen {...params} /> :
+         currentScreen === "Wifi" ? <EnterWifiScreen {...params} /> :
+         currentScreen === "Prog" ? <ProgramTagScreen {...params} /> : null}
+      </div>
     );
   }
 }
